df65895c565c18cba53cfc571e46bc068915e0a3
InsaneGamerZ <86635509+NewCoder12363@users.noreply.github.com>
2022-09-23 16:21:28 -0700

    Add files via upload

---
+++ Redirect/script.js
@@ -0,0 +1,10 @@
+var device = navigator.userAgent
+if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))
+{
+window.location = "./mobile/";
+}
+else
+{
+var page = "./desktop/";         
+top.location = page;
+} 
